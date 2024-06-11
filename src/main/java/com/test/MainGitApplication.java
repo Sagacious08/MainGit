3139b1dabeded052892f6981ebfa55dd015c07e8
Sagar Tawate <sagartawate1998@gmail.com>
2024-06-11 13:14:56 +0530

    test

---
+++ src/main/java/com/test/MainGitApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainGitApplication.class, args);
+	}
+
+}
